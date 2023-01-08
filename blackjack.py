c1673802022cecef9d04c1a81c3311c337465f0f
Stella Annor <56633439+StellaA30@users.noreply.github.com>
2023-01-08 19:33:59 +0000

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -15,29 +15,29 @@ def play():
     
     cards = init_cards()
     
-    #define two empyty lists to store the player and dealer's cards
+    # define two empyty lists to store the player and dealer's cards
     user_cards = []
     computer_cards = []
 
-    #a boolean which will be used to end the game if the dealer or player has a blackjack or if the user's score is over 21.
+    # a boolean which will be used to end the game if the dealer or player has a blackjack or if the user's score is over 21.
     end_game = False
 
 
-    #this for loop is to deal the user and dealer's 2 cards using the random_cards() funnction.
+    # this for loop is to deal the user and dealer's 2 cards using the random_cards() funnction.
     for i in range(2):
         user_cards.append(random_cards(cards))
         computer_cards.append(random_cards(cards))
 
 
     while not end_game:
-    #calculate the player and dealer score from the first two cards
+    # calculate the player and dealer score from the first two cards
         user_score = calculate_score(user_cards)
         computer_score = calculate_score(computer_cards)
         print(f'Your cards are: {user_cards} and current score is: {user_score}')
         print(f"The dealer's first card is: {computer_cards[0]}")
         
 
-        #the game ends if the dealer or the player has a blackjack (value 0) or if the player's score is over 21. 
+        # the game ends if the dealer or the player has a blackjack (value 0) or if the player's score is over 21. 
         if user_score == 21 or computer_score == 21 or user_score > 21:
             end_game = True
         else:
