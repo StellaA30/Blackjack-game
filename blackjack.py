23884de8502fb1076b0ca2a10b7669b67b2ebb90
Stella Annor <56633439+StellaA30@users.noreply.github.com>
2023-01-08 18:13:14 +0000

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -12,41 +12,44 @@ def clear():
 
 
 def play():
-    print('Hello, potential future BBC developer!')  # execution starts here! delete this line and add your game code.
+    
     cards = init_cards()
+    
+    #define two empyty list to store the user and dealer's cards
     user_cards = []
     computer_cards = []
 
-    #a boolean to end the game
+    #a boolean which will be used to end the game if the dealer or player has a blackjack or if the user's score is over 21.
     end_game = False
 
 
-    #this is for the first two cards 
+    #this for loop is to deal the user and dealer's 2 cards using the random_cards() funnction.
     for i in range(2):
         user_cards.append(random_cards(cards))
         computer_cards.append(random_cards(cards))
 
 
     while not end_game:
-    #the user and computer score from the first two cards
+    #calculate the player and dealer score from the first two cards
         user_score = calculate_score(user_cards)
         computer_score = calculate_score(computer_cards)
         print(f'Your cards are: {user_cards} and current score is: {user_score}')
         print(f"The computer's first card is: {computer_cards[0]}")
         
 
-        #the game ends if the computer or the user has a blackjack (value 0) or if the user's score is over 21.
+        #the game ends if the dealer or the player has a blackjack (value 0) or if the player's score is over 21. 
         if user_score == 21 or computer_score == 21 or user_score > 21:
             end_game = True
         else:
+            #if the game has not ended, the player can choose to 'hit' (draw a card) or 'stand' (stop drawing cards)
             user_deal = input("Type 'hit' to get another card or type 'stand' to pass: ").lower()
             if user_deal == 'hit':
                 user_cards.append(random_cards(cards))
             else:
                 end_game=True
 
-    '''Dealer strategy
-
+    '''
+    Dealer strategy
     the dealer has to hit until he/she busts or has at least 17
     
     '''
